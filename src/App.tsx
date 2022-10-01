24ce9896327416a5ee1b687a0c8ca00c6dc669c7
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-10-01 07:12:40 +0530

    initial editor with some commented modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { createEditor, Descendant } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 
-function App() {
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+] as any;
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()));
+  const [value, setValue] = useState(initialValue);
+  //const [value, setValue] = useState<Descendant[]>([]);
+
+  useEffect(()=>console.log(value), [value]);
+
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div>
+      <Slate
+        editor={editor}
+        value={value}
+        // onChange={(opts) => {
+        //   /**  eslint-disable-next-line no-lone-blocks
+        //   {opts.values === undefined || null ? setValue(opts.values) : setValue(opts.values.toString())}  **/
+        //   setValue(opts);
+        //   console.log(value);
+        // }}
+        onChange={
+          setValue
+        }
+      >
+        <Editable />
+      </Slate>
     </div>
   );
-}
+};
 
 export default App;
