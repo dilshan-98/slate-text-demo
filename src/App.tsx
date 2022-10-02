d8aa87d1766467cd435ba4bbd6f08132ac55bbe7
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-10-02 13:45:40 +0530

    seperated the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { createEditor, Descendant } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
-
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-] as any;
+import * as React from "react";
+import SyncingEditor from "./syncingEditor";
 
 const App = () => {
-  const [editor] = useState(() => withReact(createEditor()));
-  const [value, setValue] = useState(initialValue);
-  //const [value, setValue] = useState<Descendant[]>([]);
-
-  useEffect(()=>console.log(value), [value]);
-
   return (
-    <div>
-      <Slate
-        editor={editor}
-        value={value}
-        // onChange={(opts) => {
-        //   /**  eslint-disable-next-line no-lone-blocks
-        //   {opts.values === undefined || null ? setValue(opts.values) : setValue(opts.values.toString())}  **/
-        //   setValue(opts);
-        //   console.log(value);
-        // }}
-        onChange={
-          setValue
-        }
-      >
-        <Editable />
-      </Slate>
-    </div>
+    <SyncingEditor />
   );
 };
 
